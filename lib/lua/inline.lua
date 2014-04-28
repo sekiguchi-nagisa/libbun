0a5befcf5b902cf72590cf827439e07d412f2509
uchida <uchida.atsushi.8@gmail.com>
2014-04-28 19:12:27 +0900

    update lua generator
    fix while
    
    fix break node
    
    add tostring to concat string type
    
    fix IntArray
    
    fix IntMap
    
    fix string
    
    fix try
    
    fix try
    
    fix throw
    
    fix Class
    
    fix inheritance
    
    fix super
    
    work binary trees
    
    add math library

--- lib/lua/inline.lua
+++ lib/lua/inline.lua
@@ -1,17 +1,24 @@
 ## @main
 main()
 
-## @error;@SoftwareFault
+## @indexOf
+function libbun_indexOf(str, sub)
+    local index = string.find(str, sub, 1, true)
+    if index == nil then
+        return -1
+    end
+    return index - 1
+end
 
-## @Fault
-
-## @SoftwareFault;@Fault
-
-## @null
-
-## @catch;@SoftwareFault
-
-## @arraysize
-
-## @mapget
-
+## @try
+function libbun_try(functions)
+	local status, exception = pcall(functions["try"])
+	if not status then
+		if functions["catch"] then
+			functions["catch"](exception)
+		end
+	end
+	if functions["finally"] then
+		functions["finally"]()
+	end
+end
